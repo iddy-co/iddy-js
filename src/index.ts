cd025d1c28d2ca118f753550e593cd5c91c60b9e
havoc <havoc@earth.org>
2019-10-16 20:11:42 -0700

    T3

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import IddyClient from './iddyClient'
 import Authentication from './authentication'
 
-interface IIddyClientOptions {
-    domain: string
-}
-
-const createIddyClient = async (options: IIddyClientOptions) => {
-    console.log('test = createIddyClient')
-    const iddyClient = new IddyClient(options)
-    return iddyClient
-}
-
-const functionA = () => {
-    console.log('functionA - OK 3')
-}
-
-const functionB = () => {
-    console.log('functionB - OK 3')
-}
-
-export { createIddyClient, functionA, functionB, Authentication }
+export { Authentication }
