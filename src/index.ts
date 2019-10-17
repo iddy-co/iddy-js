985fb8195b933ce9d201dfcf472fc6130b65db58
havoc <havoc@earth.org>
2019-10-16 17:54:43 -0700

    T2

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ interface IIddyClientOptions {
 }
 
 const createIddyClient = async (options: IIddyClientOptions) => {
+    console.log('test = createIddyClient')
     const iddyClient = new IddyClient(options)
     return iddyClient
 }
@@ -14,4 +15,8 @@ const functionA = () => {
     console.log('functionA - OK 3')
 }
 
-export { createIddyClient, functionA, Authentication }
+const functionB = () => {
+    console.log('functionB - OK 3')
+}
+
+export { createIddyClient, functionA, functionB, Authentication }
